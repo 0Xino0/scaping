4550920094d5d51a4d1f1ecea917868bc7506064
javad <javadazizii2002@gmail.com>
2024-07-02 12:59:39 +0300

    add dataset and view to html file and create output.html

--- manage.py
+++ manage.py
@@ -8,7 +8,34 @@ def main():
     url = "https://catalog.data.gov/dataset"
     scraper = DatasetScraper()
     collection = scraper.scraper(url)
-    print(collection)
+    
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>User Information</title>
+    </head>
+    <body>
+        <h1>User Information</h1>
+    """
+    
+    
+        
+    for dataset, view in zip(collection['dataset'], collection['view']):
+        html_content += f"""
+        <span>{dataset} - {view}</span><br>
+        """
+    
+    html_content += """
+    </body>
+    </html>
+    """
+
+    with open('output.html', 'w', encoding='utf-8') as file:
+        file.write(html_content)
+        file.close()
 
 if __name__ == '__main__':
     main()
